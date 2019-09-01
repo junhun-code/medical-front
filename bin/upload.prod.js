159571d6afff808d1c7ac70771a65ae787db4eb6
徐鹏 <xupeng_zzz@163.com>
2019-09-01 17:05:12 +0800

    feat: 发布生产

---
+++ bin/upload.prod.js
@@ -0,0 +1,22 @@
+"use strict";
+const client = require("scp2");
+
+// 测试
+client.scp(
+  "./dist/",
+  {
+    host: "60.191.84.4",
+    username: "zhang",
+    password: "JZXserver2018~",
+    path: "/d:/Medical/front/"
+  },
+  function(err) {
+    if (!err) {
+      console.log(
+        "scp2工具上传完毕,prod远端服务路径：/Medical/front/"
+      );
+    } else {
+      console.log("err", err);
+    }
+  }
+);
